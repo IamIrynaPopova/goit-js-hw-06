6cf3eed35254c75d6e773700f6fec854857ad547
Iryna Popova <popovaira969@gmail.com>
2022-12-28 14:51:29 +0200

    edit

--- js/task-06.js
+++ js/task-06.js
@@ -4,14 +4,16 @@ const inputLength = Number(inputEl.dataset.length);
 console.log(inputLength);
 
 const checkInputLength = (event) => {
-  if (event.currentTarget.value.length > inputLength) {
-    return inputEl.classList.add("invalid");
+  if (event.currentTarget.value.length === inputLength) {
+    console.log(event.currentTarget.value.length);
+    inputEl.classList.remove("invalid");
+   inputEl.classList.add("valid");
+  }else
+  if (event.currentTarget.value !== inputLength) {
+    inputEl.classList.remove("valid");
+    inputEl.classList.add("invalid");
   }
-  if (event.currentTarget.value === "") {
-    return inputEl.classList.remove("valid");
-  }
-  inputEl.classList.remove("invalid");
-  return inputEl.classList.add("valid");
-};
+};  
+
 
 inputEl.addEventListener("blur", checkInputLength);
